e406c3beaf32085af30eccf48f989be5e60f11b2
fireflies <562802717@qq.com>
2019-05-16 20:35:13 +0800

    icon

---
+++ app/src/main/java/com/bnrc/bnrcbus/view/activity/base/BaseActivity.java
@@ -0,0 +1,16 @@
+
+package com.bnrc.bnrcbus.view.activity.base;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bnrc.bnrcbus.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
